ae59382f182c0a4ec6ff50c3317807050449c15e
Shahadat-Hossen-008 <penaxavier078@gmail.com>
2024-09-10 20:45:57 +0600

    Math related function , random number and problem swap value

---
+++ math.js
@@ -0,0 +1,51 @@
+// console.log(Math.abs(5-10)) // it will give positive number only
+// console.log(Math.round(2.234)) // It will give you close number without fraction
+// console.log(Math.round(2.85))
+// console.log(Math.ceil(2.201)) // It will give you the plus number 
+// console.log(Math.floor(2.987))// It will remove the fraction part
+// Pick up the random number
+// let lottery = [2, 6, 3, 8, 4, 1, 9, 5, 7];
+//  let rand = [] 
+//  for(let num of lottery){
+//     rand.push(Math.ceil(Math.random()*num))  get any array number
+// }
+// console.log(rand)
+let rand = (Math.ceil(Math.random()*10 + 10)) //It will generate the random number between 1to 1-10 because the multiple of ten 
+console.log(rand)
+
+
+//Date function
+// let today = new Date();
+// console.log(today);
+// let date = new Date('2052-10-19'); // string method
+// console.log(date.getMonth());
+// let specifiedDate = new Date(2009, 9, 14);
+// specifiedDate.setMonth(10) // change month, date ,year
+// console.log(specifiedDate.toLocaleString('en-GB')) //great format show time
+
+
+// swap value
+// let a =5;
+// let b =7;
+// console.log(a,b);
+// let temp = a;
+// a=b;
+// b=temp;
+// console.log(a,b);
+// let x = 5;
+// let y = 7;
+// console.log(x,y);
+// [x, y] = [y, x];
+// console.log(x, y);
+// x =x + y;
+// y = x - y;
+// x = x - y;
+// x = x ^ y;
+// y = x ^ y;
+// x = x ^ y;
+// console.log(x, y);
+
+
+
+
+
